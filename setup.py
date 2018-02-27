5fd3d970d65c65ee69a13e25922e4e94bebd578f
Emre Yılmaz <hello@emre.sh>
2018-02-27 16:35:48 +0300

    Version bump to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='curobot',
-    version='0.1',
+    version='0.1.1',
     packages=["curobot",],
     url='http://github.com/emre/curobot',
     license='MIT',
